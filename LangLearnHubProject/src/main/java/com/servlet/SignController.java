ec70a48af722b8fea99276ab9e3196c61a0fece0
kimnari1073 <nari1073@naver.com>
2023-11-29 09:24:05 +0900

    separete html/css
    1. html , css 분리
    2. index.html 테스트 파일 생성

--- LangLearnHubProject/src/main/java/com/servlet/SignController.java
+++ LangLearnHubProject/src/main/java/com/servlet/SignController.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
 @WebServlet("/sign.do")
 public class SignController extends HttpServlet {
 	@Override
@@ -19,9 +18,9 @@ public class SignController extends HttpServlet {
 		String id = rq.getParameter("id");
 		String pw = rq.getParameter("password");
 		String name = rq.getParameter("name");
-	    String birth = rq.getParameter("birth");
-	    String email = rq.getParameter("email");
-	    String gender = rq.getParameter("gender");
+		String birth = rq.getParameter("birth");
+		String email = rq.getParameter("email");
+		String gender = rq.getParameter("gender");
 		String role = rq.getParameter("role");
 
 		MemberDTO mDto = new MemberDTO();
@@ -31,20 +30,20 @@ public class SignController extends HttpServlet {
 		mDto.setBirth(birth);
 		mDto.setEmail(email);
 		mDto.setGender(gender);
-		mDto.setRole(role);		
-		
-		MemberDAO mDao = new MemberDAO();	
+		mDto.setRole(role);
+
+		MemberDAO mDao = new MemberDAO();
 		boolean insertCheck = mDao.memberInsert(mDto);
 
-	    if(insertCheck){
-	    	rq.setAttribute("SignResult", insertCheck);
+		if (insertCheck) {
+			rq.setAttribute("SignResult", insertCheck);
 			HttpSession session = rq.getSession();
 			session.setAttribute("idKey", id);
 			RequestDispatcher dispatcher = rq.getRequestDispatcher("index.jsp");
 			dispatcher.forward(rq, rp);
 
-		}else{
-	    	rq.setAttribute("joinResult", 0);
+		} else {
+			rq.setAttribute("joinResult", 0);
 			RequestDispatcher dispatcher = rq.getRequestDispatcher("SignUpFail.jsp");
 			dispatcher.forward(rq, rp);
 		}
