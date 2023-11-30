a31f3ee03a91a1e6891eb7cce037e2b413ffef20
sunghan <107450674+Han22233@users.noreply.github.com>
2023-11-30 15:31:05 +0900

    회원 삭제, 회원 정보 수정 추가

--- LangLearnHubProject/src/main/java/com/servlet/MemberDAO.java
+++ LangLearnHubProject/src/main/java/com/servlet/MemberDAO.java
@@ -4,13 +4,56 @@ package com.servlet;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-
+import java.sql.SQLException;
 
 import common.JDBCUtil;
 
 
 
 public class MemberDAO {
+	// 회원 삭제 
+	public void memberDelete(MemberDTO mDTO) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		conn = JDBCUtil.getConnection();
+		String query = "delete from useres where id = ?";
+		try {
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, mDTO.getId());
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			JDBCUtil.close(pstmt, conn);
+			
+		}
+	}
+	// 회원 수정 
+	public void memberUpdate(MemberDTO mDTO) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		conn = JDBCUtil.getConnection();
+		String query = "update users set password=?, email=?, name=?, phone=?, gender=?, birth=? where id = ?";
+		try {
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, mDTO.getPassword());
+			pstmt.setString(2, mDTO.getEmail());
+			pstmt.setString(3, mDTO.getName());
+			pstmt.setString(4, mDTO.getPhone());
+			pstmt.setString(5, mDTO.getGender());
+			pstmt.setString(6, mDTO.getBirth());
+			pstmt.setString(7, mDTO.getRole());
+			pstmt.setString(8, mDTO.getId());
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			JDBCUtil.close(pstmt, conn);
+			
+		}
+	}
 	//회원가입
     public boolean memberInsert(MemberDTO mDTO) {
     	Connection conn = null;
@@ -42,7 +85,8 @@ public class MemberDAO {
         }
         return flag;
     }		
-	//로그인
+    
     //
+	
 
 }
